a4204f4ca97d1e48e37538e5020dbfcc8686faa8
mriduava <mriduava@gmail.com>
2020-10-11 23:56:19 +0200

    refactor codes

--- src/com/Seat.java
+++ src/com/Seat.java
@@ -1,6 +1,7 @@
 package com;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Seat {
@@ -12,5 +13,30 @@ public class Seat {
 
     public Set<Integer> getData(){
         return this.seats;
+    }
+
+    public SeatIterator getIterator(){
+        return new SeatIterator(this);
+    }
+
+    public class SeatIterator implements Iterator<Integer> {
+        private Iterator<Integer> iterator;
+
+        public SeatIterator(Seat seat) {
+            this.iterator = seat.getData().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.iterator.hasNext();
+        }
+
+        @Override
+        public Integer next() {
+            if (this.iterator.hasNext()) {
+                return this.iterator.next();
+            }
+            return null;
+        }
     }
 }
